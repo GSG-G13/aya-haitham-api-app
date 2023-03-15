144877158bd7dbaa23609658bbdfdd9c0c55412a
Aya Qunoo <qunooa@gmail.com>
2023-03-15 14:23:50 +0200

    add giveaways functionality added generic.js  some css html edits Relates #14

---
+++ script/generic.js
@@ -0,0 +1,27 @@
+function fetch(url, cb) {
+  let xhr = new XMLHttpRequest();
+  const proxyURL = `https://api.codetabs.com/v1/proxy?quest=${url}`;
+  let returnedData;
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      returnedData = JSON.parse(xhr.responseText);
+      console.log(returnedData, "2222");
+      cb(returnedData, 20);
+    }
+  };
+  xhr.open("GET", proxyURL, true);
+
+  xhr.send();
+}
+function selectFilter(arr, cb, url, dom) {
+  let activeSelectors = arr.filter((element) => element.value);
+  let link = activeSelectors.reduce((acc, element, index) => {
+    if (index == 0) {
+      return (acc += `${element.className}=${element.value}`);
+    }
+    return (acc += `&${element.className}=${element.value}`);
+  }, `${url}?`);
+
+  console.log(link);
+  cb(link, dom);
+}
